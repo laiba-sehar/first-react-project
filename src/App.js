e67d4ee60f6618aa1c145d725c2f2f594dfe635d
Ume <seharlaiba772@gmail.com>
2024-01-17 23:59:20 +0500

    Login UI completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 
 import './App.css';
-
+import LoginForm from './Components/LoginForm/LoginForm'
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <LoginForm/>
     </div>
   );
 }
---
+++ src/Components/LoginForm/LoginForm.jsx
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+import './LoginForm.css';
+import { FaUser } from 'react-icons/fa';
+import { IoMdEye, IoMdEyeOff } from 'react-icons/io';
+
+const LoginForm = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [usernameError, setUsernameError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    // Reset previous error messages
+    setUsernameError('');
+    setPasswordError('');
+
+    // Check if username is empty
+    if (!username.trim()) {
+      setUsernameError('Please enter your username.');
+    } else if (username !== "ahmad") {
+      setUsernameError("Incorrect Username");
+    }
+
+    // Check if password is empty
+    if (!password.trim()) {
+      setPasswordError('Please enter your password.');
+    } else if (password !== "ahmad1234") {
+      setPasswordError("Incorrect Password");
+    }
+
+    // If there are errors, don't proceed with the login logic
+    if (usernameError || passwordError) {
+      return;
+    }
+
+    // Perform your login logic here
+    // ...
+
+    // Reset the form after successful login
+    setUsername('');
+    setPassword('');
+  };
+
+  return (
+    <div className="wrapper">
+      <form onSubmit={handleLogin}>
+        <h1>Login</h1>
+        <div className={`input-box ${usernameError ? 'error' : ''}`}>
+          <input
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <FaUser className="Icon1" />
+          {usernameError && <p className="error">{usernameError}</p>}
+        </div>
+        <div className={`input-box ${passwordError ? 'error' : ''}`}>
+          <input
+            type={showPassword ? 'text' : 'password'}
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          {showPassword ? (
+            <IoMdEye className="Icon2" onClick={() => setShowPassword(false)} />
+          ) : (
+            <IoMdEyeOff className="Icon2" onClick={() => setShowPassword(true)} />
+          )}
+          {passwordError && <p className="error">{passwordError}</p>}
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
